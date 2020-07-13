8858bd815e92ca069819fcc3ae3e4e55db415648
Paul Brugger <paul@brugger.com>
2020-07-13 16:50:26 -0600

    2-HTML

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Timer from "./Timer.svelte";
+  import HowTo from "./HowTo.svelte";
+</script>
+
+<style>
+  h1,
+  h3 {
+    text-align: center;
+  }
+</style>
+
+<h1>Handwashing App</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+    Picture Source
+  </a>
+  <a href="https://freesound.org/people/metrosock99/sounds/345086/">
+    Sound Source
+  </a>
+</h3>

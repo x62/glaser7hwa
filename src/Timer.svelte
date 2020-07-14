a2f79fd115805abab0d48713d6cd04c29940f424
Paul Brugger <paul@brugger.com>
2020-07-14 16:48:10 -0600

    3-Timer

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,23 +1,49 @@
 <script>
   import ProgressBar from "./ProgressBar.svelte";
+  const totalSeconds = 20;
+  let secondLeft = totalSeconds;
+  let isRunning = false;
+  $: progress = ((totalSeconds - secondLeft) / totalSeconds) * 100;
+
+  function startTimer() {
+    isRunning = true;
+    const timer = setInterval(() => {
+      secondLeft -= 1;
+      if (secondLeft == 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondLeft = totalSeconds;
+      }
+    }, 1000);
+  }
 </script>
 
 <style>
   h2 {
     margin: 0;
   }
-
   .start {
     background-color: rgb(154, 73, 73);
     width: 100%;
     margin: 10px 0;
   }
+  .start[disabled] {
+    background-color: rgb(194, 194, 194);
+    cursor: not-allowed;
+  }
 </style>
 
 <div bp="grid">
-  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondLeft}</h2>
 </div>
+<ProgressBar {progress} />
 
-<ProgressBar />
-
-<button class="start">Start</button>
+<div bp="grid">
+  <button
+    disabled={isRunning}
+    on:click={startTimer}
+    bp="offset-5@md 4@md 12@sm"
+    class="start">
+    Start
+  </button>
+</div>
